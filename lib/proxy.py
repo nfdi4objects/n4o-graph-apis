c709872f8cb51c074c92a770a67c63688e1bd8bd
Josef Heers <josef.heers@gbv.de>
2025-11-20 07:54:20 +0000

    Fix: request lacks json data

--- lib/proxy.py
+++ lib/proxy.py
@@ -22,7 +22,7 @@ def enable_proxy(app, backend, base):
         elif request.method == 'DELETE':
             resp = requests.delete(target_url, headers=request.headers, data=request.data)
         elif request.method == 'POST':
-            resp = requests.post(target_url, data=request.form, files=request.files)
+            resp = requests.post(target_url, json=request.json, data=request.form, files=request.files)
         else:
             resp = make_response(f'Not supported method {request.method}', 500)
         return Response(resp.content, resp.status_code, cleanHeaders(resp))
